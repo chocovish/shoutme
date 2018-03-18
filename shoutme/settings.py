6cecaedddcfddbfd63eb66035e8b1e4e0dc03196
Vishal ghosh <vishal.ghosh28@gmail.com>
2018-03-18 11:10:28 +0000

    asas

--- shoutme/settings.py
+++ shoutme/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'n&^)*9+#-j0li#z)x30oj^d@$n_pmb&v3-xyxupaq%6(o5w4cr'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["*"]
 
